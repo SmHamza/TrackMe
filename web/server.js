18426bac8370838e6f226e2ce5955b09a7acdccd
SmHamza <smhamza1312@gmail.com>
2020-08-25 19:59:33 +1000

    changed headers

--- web/server.js
+++ web/server.js
@@ -6,6 +6,13 @@ app.use(express.static('public'));
 app.use((req, res, next) => {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header ("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
+    res.header ("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+    res.header ("Allow: GET, POST, OPTIONS, PUT, DELETE");
+    $method = $_SERVER ['REQUEST_METHOD'];
+    if ($method == "OPTIONS") {
+        die ();
+    }
     next();
 });
 app.get('/', function (req, res) { 
